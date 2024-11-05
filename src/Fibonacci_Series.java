d3a534443f7bd1c6416e8aacbfbc90a7fd02a963
Abhishek SIngh <abhishekmithileshsingh1@gmail.com>
2024-11-05 21:54:51 +0530

    Fibonacci_Series

---
+++ src/Fibonacci_Series.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Fibonacci_Series {
+    public static void main (String [] args){
+
+      int n1 = 0;
+      int n2 = 1;
+
+      Scanner obj = new Scanner(System.in);
+      System.out.print("Enter the Integer for Fibonacci Series  : ");
+      int number = obj.nextInt();
+        System.out.print(" Fibonacci Series upto " + number+" terms : ");
+      for(int i = 1 ; i<=number; i++){
+
+          System.out.print(n1+" ");
+          int nextTerm = n1 + n2;
+          n1 = n2;
+          n2= nextTerm;
+      }
+    }
+
+}
